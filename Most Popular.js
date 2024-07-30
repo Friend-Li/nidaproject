89ce64a1f4e3fb3b67482a0a9f650b6b57bda07a
Friend-Li <72420511+Friend-Li@users.noreply.github.com>
2024-07-29 17:13:18 -0700

    Michael's Files
    Hey - The Front-end team

--- Most Popular.js
+++ Most Popular.js
@@ -1,14 +1,3 @@
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
-});
-
-sr.reveal('.jewelery-text', {delay:200, origin:'top'});
-sr.reveal('.jewelery-logo', {delay:450, origin:'top'});
-sr.reveal('.icons', {delay:450, origin:'left'});
-
 let scrollUp = document.querySelector('.scroll-up');
 let scrollUpBar = document.getElementById('scroll-up-link');
 
@@ -21,7 +10,3 @@ scrollUp.addEventListener("click", function() {
     }
     });
         
-  
-
-
-
