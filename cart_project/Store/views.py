a5fe57bd086225c517c6bc46fd1058dfb2e33182
Ecm01 <157766150+Ecm01@users.noreply.github.com>
2024-07-31 23:11:03 -0700

    Update views.py
    it is now working

--- cart_project/Store/views.py
+++ cart_project/Store/views.py
@@ -25,6 +25,12 @@ def add_to_cart(request):
     data = json.load(request.body)
     product_id = data["id"]
     product = Product.objects.get(id=product_id)
+    if request.user.is_authenticated:
+       cart, created = Cart.objects.get_or_create(user=request.user, completed=False)
+       cartitem, created = CartItem.objects.get_or_create(cart=cart, product=product)
+       cartitem.quantity += 1
+       cartitem.save()
+       print(cartitem)
     return JsonResponse("it worked", safe=False)
 
 
