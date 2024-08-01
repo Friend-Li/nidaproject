905d7ba2780e2104c15b066e927ccd958f2a5a22
Ecm01 <157766150+Ecm01@users.noreply.github.com>
2024-07-31 21:34:57 -0700

    Update script.js
    maybe works at this point

--- cart_project/Store/static/js/script.js
+++ cart_project/Store/static/js/script.js
@@ -1,19 +1,19 @@
-#function getCookie(name) {
-#    let cookieValue = null;
-#    if (document.cookie && document.cookie !== '') {
-#        const cookies = document.cookie.split(';');
-#        for (let i = 0; i < cookies.length; i++) {
-#            const cookie = cookies[i].trim();
-#            // Does this cookie string begin with the name we want?
-#            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-#                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-#                break;
-#            }
-#        }
-#    }
-#    return cookieValue;
-#}
-#const csrftoken = getCookie('csrftoken');
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
 
 
 
@@ -34,13 +34,17 @@ function addToCart(e){
 
     fetch(url, {
         method: "POST",
-        headers: {"Content-Type":"application/json"},
+        headers: {"Content-Type":"application/json", 'X-CSRFToken': csrftoken},
         body:JSON.stringify(data
-    })        
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+    })
+    .catch(error=>{
+        console.log(error)
+    }
 }
 
-#    })
-#    .then(res=>res.json())
-#    .then(data=>{
-#        console.log(data)
-}
+
+
