d4360403f43441b65cf69a3456762ff0627c4651
Ecm01 <157766150+Ecm01@users.noreply.github.com>
2024-09-01 16:30:53 -0700

    Update script.js

--- cart_project/Store/static/js/script.js
+++ cart_project/Store/static/js/script.js
@@ -1,50 +1,13 @@
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+
+	})
 }
-const csrftoken = getCookie('csrftoken');
-
-
-
-
-
-
-let btns= document.querySelectorAll(".productContainer button")
-
-btns.forEach(btn=>{
-    btn.addEventListener("click", addToCart)
-})
-
-function addToCart(e){
-    let product_id = e.target.value
-    let url = "/add_to_cart"
-
-    let data = {id:product_id}
-
-    fetch(url, {
-        method: "POST",
-        headers: {"Content-Type":"application/json", 'X-CSRFToken': csrftoken},
-        body:JSON.stringify(data)
-    })
-    .then(res=>res.json())
-    .then(data=>{
-        console.log(data)
-    })
-    .catch(error=>{
-        console.log(error)
-    })
-} 
 
 
 
