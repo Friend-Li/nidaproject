56b5f8160587566965631e6366306976354734f1
Ecm01 <157766150+Ecm01@users.noreply.github.com>
2024-09-01 17:16:03 -0700

    Update urls.py

--- cart_project/Store/urls.py
+++ cart_project/Store/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("cart", views.cart, name="cart"),
-    path("add_to_cart", views.add_to_cart, name= "add")
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
+    
+
 ]
-#This path will go to the code above
-#path("confirm_payment/<str>pk", views.cofirm_payment, name="add")
+    
+
+]
