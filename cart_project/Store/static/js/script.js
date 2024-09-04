1498da6d261e28fb5fb4fee24f9d33979e70f080
Ecm01 <157766150+Ecm01@users.noreply.github.com>
2024-09-03 18:15:06 -0700

    Update script.js

--- cart_project/Store/static/js/script.js
+++ cart_project/Store/static/js/script.js
@@ -8,13 +8,37 @@ for (i = 0; i < updateBtns.length; i++) {
 
 		console.log('USER:', user)
 		if(user === 'AnonymousUser'){
-			console.log('Not logged in')
+			addCookieItem(productId, action)
 		}else{
 			updateUserOrder(productId, action)
 		}
 
 	})
-}	
+}
+
+function addCookieItem(productId, action){
+	console.log('Not logged in..')
+
+	if(action == 'add'){
+		if(cart[productId] == undefined){
+			cart[productId] = {'quantity':1}
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+	if(action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if(cart[productId]['quantity'] <= 0){
+			console.log('Remove item')
+			delete cart[productId]
+		}
+	}
+	console.log('Cart:', cart)
+	document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	location.reload()
+}
+
 
 function updateUserOrder(productId, action){
 	console.log('User is logged in, sending data..')
@@ -26,17 +50,14 @@ function updateUserOrder(productId, action){
 		headers:{
 			'Content-Type':'application/json',
 			'X-CSRFToken':csrftoken,
-		},
-		body:JSON.stringify({'productId': productId, 'action': action})
+		}, 
+		body:JSON.stringify({'productId':productId, 'action':action})
 	})
-
-	.then((response) =>{
-		return response.json()
+	.then((response) => {
+	   return response.json();
 	})
-	.then((data) =>{
+	.then((data) => {
 		console.log('data:', data)
-	})
+		location.reload()
+	});
 }
-
-
-
